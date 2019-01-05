c72631c7b6e549b2dd41b110200177cbad0c5ebe
Moustafa Elsisy <spaceflyby@gmail.com>
2019-01-05 18:29:52 -0330

    Added support for web sockets and transferring html files

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express")
+const http = require("http")
+const socketio = require("socket.io")
 const controllers = require("./controllers")
 
 const app = express()
 app.use(controllers)
-app.listen(3000, () => console.log("Running on port 3000..."))
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on("connection", client => {
+    console.log("Client connected")
+    client.on("disconnect", () => console.log("Client disconnected"))
+})
+
+server.listen(3000, () => console.log("Running on port 3000..."))
--- controllers/test.js
+++ controllers/test.js
@@ -1,9 +1,10 @@
 const express = require("express")
+const path = require("path")
 const router = express.Router()
 
 router.get("/", (req, res) => {
     console.log("Test Controller base path called")
-    res.send("Hello world!")
+    res.sendFile(path.resolve(`${__dirname}/../views/index.html`))
 })
 
 module.exports = router
