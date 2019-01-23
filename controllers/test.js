56d724de3072fdccfd19e1d8e5fa392e3e3645fe
Moustafa Elsisy <spaceflyby@gmail.com>
2019-01-23 19:22:50 -0330

    Added a db service, and showed example usage in the test controller

--- controllers/test.js
+++ controllers/test.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const path = require("path")
+const { db, mongoUid } = require("../services/db")
+
 const router = express.Router()
 
 router.get("/", (req, res) => {
     console.log("Test Controller base path called")
+
+    db.test.findOne({
+        _id: mongoUid('5c48e9171c9d4400000b5499')
+    }, function(err, doc) {
+        console.log(doc)
+        if(err) console.error(err)
+    })
+
     res.sendFile(path.resolve(`${__dirname}/../views/index.html`))
 })
 
---
+++ services/db.js
@@ -0,0 +1,12 @@
+const config = require("../config")
+const mongojs = require("mongojs")
+
+const { user, password, clusterString, database, options } = config.db
+const queryParams = Object.keys(options).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(options[k])}`).join('&')
+const mongoUrl = `mongodb://${user}:${password}@${clusterString}/${database}?${queryParams}`
+const db = mongojs(mongoUrl)
+
+module.exports = {
+    db,
+    mongoUid: mongojs.ObjectID
+}
