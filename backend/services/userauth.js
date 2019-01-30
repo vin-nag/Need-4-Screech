56ebb2c370a336250052b72958b02b764ef0eeca
Liam Reardon <lmr647@mun.ca>
2019-01-29 20:42:33 -0330

    user auth -WIP

--- backend/services/userauth.js
+++ backend/services/userauth.js
@@ -1,21 +1,84 @@
+var db = require('./db');
+
+// user model
+function user(username, email, password) {
+
+	this.username = username;
+	this.email = email;
+	this.password = password;
+}
+
+
 function login() {
 
-	var username = document.getElementById('loginUsername');
-	var password = document.getElementById('loginPassword');
+	var username = document.getElementById('loginUsername').value;
+	var password = document.getElementById('loginPassword').value;
 
-	alert(username, password);
+	
 }
 
 function signUp() {
 
-	var username = document.getElementById('signUpUsername');
-	var email = document.getElementById('signUpEmail');
-	var password = document.getElementById('signUpPassword');
-	var confirmPass = document.getElementById('signUpConfirm');
+	var username = document.getElementById('signUpUsername').value;
+	var email = document.getElementById('signUpEmail').value;
+	var password = document.getElementById('signUpPassword').value;
+	var confirmPass = document.getElementById('signUpConfirm').value;
 
-	alert(username, password, email, confirmPass);
+
+	if (username === "" || email === "" || password === "" || confirmPass === "") {
+
+		alert("one or more fields are empty");
+	}
+
+	if (password.length < 6) {
+	
+		alert("password must be at least 6 characters");
+	}
+
+
+	if (username.length < 4) {
+
+		alert("username must be 4 or more characters");
+	}
+
+	if (password !== confirmPass) {
+
+		alert("passwords do not match");
+	}
+
+	if (validateEmail(email) === false) {
+
+		 alert("incorrect email");
+	}
+
+	else {
+
+		user = new user(username, email, password);
+		registerUser(user);
+	}
 
 }
+
+// function to validate email in format anystring@anything.anystring
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function registerUser(user) {
+
+	db.users.ensureIndex({email : 1}, {unique : true});
+	db.users.ensureIndex({username : 1}, {unique : true});
+	db.users.save(user, function(err, savedUser) {
+
+		if (err || !savedUser) { console.log("User " + user.email + " was not saved because of " + err)}
+		else { console.log("User saved successfully!")}
+
+	})
+
+}
+
+
 
 
 
