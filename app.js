c067fd5c39a59a5dcbda742ae520ae903c803716
Liam Reardon <lmr647@mun.ca>
2019-01-31 16:54:05 -0330

    save users to database working

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path")
 const socketio = require("socket.io")
 const httpControllers = require("./backend/controllers/http")
 const socketControllers = require("./backend/controllers/socket")
+const userAuth = require("./backend/services/userauth")
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'frontend/js/dist'))) //static resource
@@ -15,3 +16,20 @@ const io = socketio(server)
 socketControllers.listen(io)
 
 server.listen(3000, () => console.log("Running on port 3000..."))
+
+io.sockets.on('connection', function(socket){
+    console.log('socket connection');
+ 
+    socket.on('onSignUp',function(data){
+
+        console.log(data.username);
+        console.log(data.email);
+        console.log(data.password);
+        console.log(data.confirmPass);
+
+        userAuth.signUp(data.username, data.email, data.password, data.confirmPass);
+       
+    });
+   
+});
+
--- backend/services/db.js
+++ backend/services/db.js
@@ -1,11 +1,10 @@
 const config = require("../../config")
 const mongojs = require("mongojs")
-const collections = ['users'];
 
 const { user, password, clusterString, database, options } = config.db
 const queryParams = Object.keys(options).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(options[k])}`).join('&')
 const mongoUrl = `mongodb://${user}:${password}@${clusterString}/${database}?${queryParams}`
-const db = mongojs(mongoUrl, collections)
+const db = mongojs(mongoUrl, ['users'])
 
 module.exports = {
     db,
