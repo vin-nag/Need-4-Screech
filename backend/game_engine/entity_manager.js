074fa6acba72a9b6e1b6431681a5698242d2292c
vin-nag <vnagisetty@mun.ca>
2019-02-27 16:18:39 -0330

    tag can be null

---
+++ backend/game_engine/entity_manager.js
@@ -0,0 +1,73 @@
+/**
+ * Entity Manager class using standard ECS architecture.
+ */
+
+class EntityManager {
+
+    constructor() {
+        this.id = 0;
+        this.entitiesMap = {};
+        this.entitiesToAddMap = {};
+        this.size = 0;
+    }
+
+    addEntity(tag=null){
+        /** this function adds entities.
+         * @param {string} tag type of the component
+         * */
+
+        let entity = new Entity(this.id, tag);
+        this.id++;
+
+        this.entitiesToAddMap[entity.id] = entity;
+    }
+
+    removeInactiveEntities(){
+        /** this function removes inactive entities.
+         * @param {string} tag type of the component
+         * */
+
+        for (var entity in this.entitiesMap) {
+            if (entity.active === false) {
+                delete this.entitiesMap[entity];
+            }
+        }
+    }
+
+    getEntities(){
+        // standard getter function
+        return this.entitiesMap;
+    }
+
+    update(){
+        /** this function does an update, adding entities and removing them.
+         * */
+
+        // add entities to entity map
+        for (var entity in this.entitiesToAddMap) {
+
+            if (entity.id in this.entitiesMap){
+                this.id++;
+                console.log('entities are overlapping');
+                entity.id = this.id;
+            }
+            this.entitiesMap[entity.id] = entity;
+        }
+
+        // clear entitiesToAddMap
+        this.entitiesToAddMap = {};
+
+        // remove inactive entities
+        this.removeInactiveEntities();
+
+        // update size
+        this.size = Object.keys(this.entitiesMap).length;
+    }
+
+    getSize(){
+        // standard getter function
+        return this.size;
+    }
+
+
+}
