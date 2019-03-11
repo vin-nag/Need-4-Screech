18e27f84a6c506c842ed4181fc4bf02c9ac5e663
vin-nag <vnagisetty@mun.ca>
2019-03-11 16:30:34 -0230

    updated input.js to pass on gamesession inputs

--- backend/controllers/socket/input.js
+++ backend/controllers/socket/input.js
@@ -1,66 +1,14 @@
 //TODO: Delegate input => CInput mapping to the gameEngine
 //TODO: Use data.gameSessionId and pass it to the GameSessionService
 
-const gamePlayState = require("../../game_engine/gameEngine");
-const player = gamePlayState.player;
-const update = gamePlayState.update;
-const gameState = gamePlayState.returnGameState();
+const gameSessionService = require("./../../services/GameSessionService");
 
 const onKeyDown = (socket, data) => {
-    let CInput = player.getComponent('CInput');
-    if (data.keyDown == 87) {
-        CInput.up = true;
-        console.log("W Pressed")
-    }
-    if (data.keyDown == 65) {
-        CInput.left = true;
-        console.log("A Pressed")
-    }
-    if (data.keyDown == 83) {
-        CInput.down = true;
-        console.log("S Pressed")
-    }
-    if (data.keyDown == 68) {
-        CInput.right = true;
-        console.log("D Pressed")
-    }
-    if (data.keyDown == 32) {
-        CInput.shoot = true;
-        console.log("Space Pressed")
-    }
-    update();
-    console.log(player.getComponent("CInput"));
-    
-}
+    let session = gameSessionService.getSession(data.sessionId);
+    session.lastInput = {event:data.event, key:data.key};
+};
 
 const onKeyUp = (socket, data) => {
-    let CInput = player.getComponent('CInput');
-    if (data.keyUp == 87) {
-        CInput.up = false;
-        console.log("W Released")
-    }
-    if (data.keyUp == 65) {
-        CInput.left = false;
-        console.log("A Released")
-    }
-    if (data.keyUp == 83) {
-        CInput.down = false;
-        console.log("S Released")
-    }
-    if (data.keyUp == 68) {
-        CInput.right = false;
-        console.log("D Released")
-    }
-    if (data.keyUp == 32) {
-        CInput.shoot = false;
-        console.log("Space Released")
-    }
-
-    update();
-    console.log(player.getComponent("CInput"));
-}
-
-module.exports = {
-    onKeyDown,
-    onKeyUp
-}
+    let session = gameSessionService.getSession(data.sessionId);
+    session.lastInput = {event:data.event, key:data.key};
+};
