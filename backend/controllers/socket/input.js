5f4fc272614098c87abd10e0b2a0f062b9bddf7f
vin-nag <vnagisetty@mun.ca>
2019-03-11 15:45:49 -0230

    Made game play module a game engine class

--- backend/controllers/socket/input.js
+++ backend/controllers/socket/input.js
@@ -1,7 +1,7 @@
 //TODO: Delegate input => CInput mapping to the gameEngine
 //TODO: Use data.gameSessionId and pass it to the GameSessionService
-    
-const gamePlayState = require("./../../game_engine/game_play");
+
+const gamePlayState = require("../../game_engine/gameEngine");
 const player = gamePlayState.player;
 const update = gamePlayState.update;
 const gameState = gamePlayState.returnGameState();
