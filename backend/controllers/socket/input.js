05be11f4c11826e5c0901fa450699d6d27b7f0e8
Liam Reardon <lmr647@mun.ca>
2019-03-12 17:28:00 -0230

    refactor handleKeyPress

--- backend/controllers/socket/input.js
+++ backend/controllers/socket/input.js
@@ -7,56 +7,17 @@ const update = gamePlayState.update;
 const gameState = gamePlayState.returnGameState();
 
 const onKeyDown = (socket, data) => {
-    //let CInput = player.getComponent('CInput');
-    if (data.keyDown == 87) {
-        //CInput.up = true;
-        console.log("W Pressed")
-    }
-    if (data.keyDown == 65) {
-       //CInput.left = true;
-        console.log("A Pressed")
-    }
-    if (data.keyDown == 83) {
-        //CInput.down = true;
-        console.log("S Pressed")
-    }
-    if (data.keyDown == 68) {
-        //CInput.right = true;
-        console.log("D Pressed")
-    }
-    if (data.keyDown == 32) {
-        //CInput.shoot = true;
-        console.log("Space Pressed")
-    }
-    update();
-    console.log(player.getComponent("CInput"));
-    
+
+    // console log keyCode
+    console.log(data.keyCode)
+   
 }
 
 const onKeyUp = (socket, data) => {
-    //let CInput = player.getComponent('CInput');
-    if (data.keyUp == 87) {
-        //CInput.up = false;
-        console.log("W Released")
-    }
-    if (data.keyUp == 65) {
-        //CInput.left = false;
-        console.log("A Released")
-    }
-    if (data.keyUp == 83) {
-        //CInput.down = false;
-        console.log("S Released")
-    }
-    if (data.keyUp == 68) {
-        //CInput.right = false;
-        console.log("D Released")
-    }
-    if (data.keyUp == 32) {
-       // CInput.shoot = false;
-        console.log("Space Released")
-    }
-
-    update();
+   
+    // console log keyCode 
+    console.log(data.keyCode)
+ 
 }
 
 module.exports = {
--- frontend/js/src/gamePlay/gamePlay.js
+++ frontend/js/src/gamePlay/gamePlay.js
@@ -12,73 +12,17 @@ class GamePlay {
     }
 
     handleKeyPress(event) {
-        // stub
         if (event.type === 'keydown') {
-            if(event.keyCode === 87) {
-                // w pressed
-                socketClient.emit('onKeyDown', {
-                    keyDown: 87
-                })
-            }
-            if(event.keyCode === 65) {
-                // a pressed
-                socketClient.emit('onKeyDown', {
-                    keyDown: 65
-                })
-            }
-            if(event.keyCode === 83) {
-                // s pressed
-                socketClient.emit('onKeyDown', {
-                    keyDown: 83
-                })
-            }
-            if(event.keyCode === 68) {
-                // d pressed
-                socketClient.emit('onKeyDown', {
-                    keyDown: 68
-                })
-            }
-            if(event.charCode === 32) {
-                // space pressed
-                socketClient.emit('onKeyDown', {
-                    keyDown: 32
-                })
-            }
+            socketClient.emit('onKeyDown', {
+                keyCode: event.keyCode
+            })   
         }
-
-        if (event.type === 'keyup') {
-            if(event.keyCode === 87) {
-                // w up
-                socketClient.emit('onKeyUp', {
-                    keyUp: 87
-                })
-            }
-            if(event.keyCode === 65) {
-                // a up
-                socketClient.emit('onKeyUp', {
-                    keyUp: 65
-                })
-            }
-            if(event.keyCode === 83) {
-                // s up
-                socketClient.emit('onKeyUp', {
-                    keyUp: 83
-                })
-            }
-            if(event.keyCode === 68) {
-                // d up
-                socketClient.emit('onKeyUp', {
-                    keyUp: 68
-                })
-            }
-            if(event.keyCode === 32) {
-                // space up
-                socketClient.emit('onKeyUp', {
-                    keyDown: 32
-                })
-            }
-        }   
-    }
+        else if (event.type === 'keyup') {
+            socketClient.emit('onKeyUp', {
+                keyCode: event.keyCode
+            })
+        }
+    }                       
 }
 
 export default GamePlay
