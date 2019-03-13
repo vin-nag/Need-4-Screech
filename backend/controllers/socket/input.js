244b401247de3d5da3e79718505b1c15406db3d6
vin-nag <vnagisetty@mun.ca>
2019-03-13 15:31:49 -0230

    exporting modules

--- backend/controllers/socket/input.js
+++ backend/controllers/socket/input.js
@@ -1,6 +1,3 @@
-//TODO: Delegate input => CInput mapping to the gameEngine
-//TODO: Use data.gameSessionId and pass it to the GameSessionService
-
 const gameSessionService = require("./../../services/GameSessionService");
 
 const onKeyDown = (socket, data) => {
@@ -12,3 +9,8 @@ const onKeyUp = (socket, data) => {
     let session = gameSessionService.getSession(data.sessionId);
     session.lastInput = {event:'onKeyUp', key:data.keyCode};
 };
+
+module.exports = {
+    onKeyDown,
+    onKeyUp
+}
