d0c59a8048f5c9bcbaa69b57f5c674bff0fba250
Liam Reardon <lmr647@mun.ca>
2019-03-13 16:07:56 -0230

    PR fix

--- backend/controllers/socket/input.js
+++ backend/controllers/socket/input.js
@@ -1,13 +1,13 @@
 const gameSessionService = require("./../../services/GameSessionService");
 
 const onKeyDown = (socket, data) => {
-    let session = gameSessionService.getSession(data.sessionId);
-    //session.lastInput = {event:'onKeyDown', key:data.keyCode};
+    let session = gameSessionService.getSession(data.sessionID);
+    session.lastInput = {event:'onKeyDown', key:data.keyCode};
 };
 
 const onKeyUp = (socket, data) => {
-    let session = gameSessionService.getSession(data.sessionId);
-    //session.lastInput = {event:'onKeyUp', key:data.keyCode};
+    let session = gameSessionService.getSession(data.sessionID);
+    session.lastInput = {event:'onKeyUp', key:data.keyCode};
 };
 
 const onNewSession = (socket, data) => {
