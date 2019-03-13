ab29cdf3a8de40feeed6df3599ec070379143339
vin-nag <vnagisetty@mun.ca>
2019-03-13 15:29:03 -0230

    modified key events to reflect gameengine

--- backend/controllers/socket/input.js
+++ backend/controllers/socket/input.js
@@ -5,10 +5,10 @@ const gameSessionService = require("./../../services/GameSessionService");
 
 const onKeyDown = (socket, data) => {
     let session = gameSessionService.getSession(data.sessionId);
-    session.lastInput = {event:data.event, key:data.key};
+    session.lastInput = {event:'onKeyDown', key:data.keyCode};
 };
 
 const onKeyUp = (socket, data) => {
     let session = gameSessionService.getSession(data.sessionId);
-    session.lastInput = {event:data.event, key:data.key};
+    session.lastInput = {event:'onKeyUp', key:data.keyCode};
 };
