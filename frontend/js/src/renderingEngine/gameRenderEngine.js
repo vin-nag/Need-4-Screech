31720b6323520edd6a0d2132f4aeea92181c01bc
vin-nag <vnagisetty@mun.ca>
2019-03-22 00:28:54 -0230

    Bars follow viewport now (needs to be refactored later on) Started adding functionality for bounding boxes to be visible

--- frontend/js/src/renderingEngine/gameRenderEngine.js
+++ frontend/js/src/renderingEngine/gameRenderEngine.js
@@ -8,16 +8,17 @@ const engine = (entities, canvasID) => {
     ctx.setTransform(1,0,0,1,0,0); //reset the transform matrix as it is cumulative
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img,0,0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-
+    let camX = 0
+    let camY = 0
 
     for(let entity of entities) {
         if (entity.tag === "player") {
 
             let playerPos = entity.componentMap['CTransform'].position;
             //Clamp the camera position to the world bounds while centering the camera around the player
-            let camX = canvasService.clamp(-playerPos.x + canvas.width/2, -5000, 5000 - canvas.width);
-            let camY = canvasService.clamp(-playerPos.y + canvas.height/2, 0, 720 - canvas.height);
-
+            camX = canvasService.clamp(-playerPos.x + canvas.width/2, -5000, 5000 - canvas.width);
+            camY = canvasService.clamp(-playerPos.y + canvas.height/2, 0, 720 - canvas.height);
+            console.log(camX);
             // set boundary for camera movement (need to add level end boundary also)
             // -637 is around the midpoint of screen, need this so canvas only starts translating at this point
             // instead of at (0,0)
@@ -32,14 +33,12 @@ const engine = (entities, canvasID) => {
             
         }
         
-        drawEntity(ctx, entity)
+        drawEntity(ctx, entity, camX, camY)
     }  
                                          
 }
 
-const drawEntity = (ctx, entity) => {
-
-
+const drawEntity = (ctx, entity, camX=0, camY=0) => {
     const animation = entity.componentMap["CAnimation"]
     const transform = entity.componentMap["CTransform"]
     const img = assetManager.getAnimationImage(animation.animName)
@@ -68,66 +67,24 @@ const drawEntity = (ctx, entity) => {
     }
 
     else {
-        ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, transform.position.x, transform.position.y, frameWidth, frameHeight)
-    }
+        if (entity.tag === "bar"){
 
-    if ("CBar" in entity.componentMap){
-        const values = entity.componentMap["CBar"];
-        const state = entity.componentMap["CState"];
-        let offsetX = 200;
-        let offsetY = 75;
-        canvasService.draw.text(ctx, "Current " + state.state + ": " + values.value, transform.position.x + offsetX, transform.position.y + offsetY, "18px", "#fff", "Permanent Marker")
-    }
+            ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, Math.max(transform.position.x, transform.position.x-camX), transform.position.y, frameWidth, frameHeight);
 
+            if ("CBar" in entity.componentMap){
+                const values = entity.componentMap["CBar"];
+                const state = entity.componentMap["CState"];
+                let offsetX = 200;
+                let offsetY = 75;
+                canvasService.draw.text(ctx, "Current " + state.state + ": " + values.value, Math.max(transform.position.x + offsetX, transform.position.x + offsetX - camX), transform.position.y + offsetY, "18px", "#fff", "Permanent Marker")
+            }
 
-    /*
-    if (entity.tag === "bar"){
-        const time = entity.componentMap["CBar"];
-        const position = entity.componentMap["CTransform"].position;
-        canvasService.draw.text(ctx, "Time Left: " + time.time, position.x, position.y, "36px", "#fff", "Permanent Marker")
-    }
-
-    else {
-        const animation = entity.componentMap["CAnimation"]
-        const transform = entity.componentMap["CTransform"]
-        const img = assetManager.getAnimationImage(animation.animName)
-        const currentFrame = Math.floor(animation.currentFrame);
-        const frameWidth = img.width / animation.numOfFrames;
-        const frameHeight = img.height;
-
-        /*
-        const bounding = entity.componentMap["CBoundingBox"]
-        canvasService.draw.rectangle(ctx, transform.position.x, transform.position.y, bounding.size.x, bounding.size.y, "#ffffff")
-
-
-        if ("CHealth" in entity.componentMap && entity.componentMap["CHealth"].show === true){
-            let currentHealthPercentage = entity.componentMap["CHealth"].health / entity.componentMap["CHealth"].maxHealth;
-            canvasService.draw.rectangle(ctx, transform.position.x - 2, transform.position.y - 10, frameWidth + 4, 8, "#860b08");
-            canvasService.draw.rectangle(ctx, transform.position.x, transform.position.y - 8, currentHealthPercentage * frameWidth, 6, "#f43a26")
         }
-
-        // drawing reverse
-        if (transform.scale === -1){
-            ctx.save();
-            ctx.translate(transform.position.x, transform.position.y);  //location on the canvas to draw your sprite, this is important.
-            ctx.scale(-1, 1);  //This does your mirroring/flipping
-            ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, -frameWidth, 0, frameWidth, frameHeight);
-            ctx.restore();
-        }
-
         else {
-            ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, transform.position.x, transform.position.y, frameWidth, frameHeight)
+        ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, transform.position.x, transform.position.y, frameWidth, frameHeight)
         }
+
     }
-    */
-
-
-
-/*
-const bounding = entity.componentMap["CBoundingBox"]
-canvasService.draw.rectangle(ctx, transform.position.x, transform.position.y, bounding.size.x, bounding.size.y, "#ffffff")
-*/
-
 
 }
 
