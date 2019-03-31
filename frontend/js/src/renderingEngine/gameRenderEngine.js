1b223bf317bd2139e738c9074efeeeeed2d25a17
Liam Reardon <lmr647@mun.ca>
2019-03-31 02:11:49 -0230

    add powerup banners

--- frontend/js/src/renderingEngine/gameRenderEngine.js
+++ frontend/js/src/renderingEngine/gameRenderEngine.js
@@ -87,28 +87,23 @@ const drawEntity = (ctx, entity, camX=0, camY=0) => {
         }
 
         if (entity.tag === "speed_text") {
-            canvasService.draw.text(ctx, "Speed++", Math.max(transform.position.x, transform.position.x - camX), transform.position.y, "45px", "#00e640", "Permanent Marker")
-            canvasService.draw.rectangle(ctx, Math.max(transform.position.x, transform.position.x - camX), transform.position.y, 235, 50, "rgba(0, 0, 0, 0.8)")
+            ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, Math.max(transform.position.x, transform.position.x-camX), transform.position.y, frameWidth, frameHeight);
         }
 
         if (entity.tag === "invincibility_text") {
-            canvasService.draw.text(ctx, "Invincibility Mode", Math.max(transform.position.x, transform.position.x - camX), transform.position.y, "45px", "#a537fd", "Permanent Marker")
-            canvasService.draw.rectangle(ctx, Math.max(transform.position.x, transform.position.x - camX), transform.position.y, 235, 50, "rgba(0, 0, 0, 0.8)")
+            ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, Math.max(transform.position.x, transform.position.x-camX), transform.position.y, frameWidth, frameHeight);
         }
 
         if (entity.tag === "shield_text") {
-            canvasService.draw.text(ctx, "Shield Powerup", Math.max(transform.position.x, transform.position.x - camX), transform.position.y, "45px", "#22a7f0", "Permanent Marker")
-            canvasService.draw.rectangle(ctx, Math.max(transform.position.x, transform.position.x - camX), transform.position.y, 235, 50, "rgba(0, 0, 0, 0.8)")
+            ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, Math.max(transform.position.x, transform.position.x-camX), transform.position.y, frameWidth, frameHeight);
         }
 
         if (entity.tag === "drunk_text") {
-            canvasService.draw.text(ctx, "DrUnK MoDe", Math.max(transform.position.x, transform.position.x - camX), transform.position.y, "45px", "#f39c12", "Permanent Marker")
-            canvasService.draw.rectangle(ctx, Math.max(transform.position.x, transform.position.x - camX), transform.position.y, 235, 50, "rgba(0, 0, 0, 0.9)")
+            ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, Math.max(transform.position.x, transform.position.x-camX), transform.position.y, frameWidth, frameHeight);
         }
 
         if (entity.tag === "health_text") {
-            canvasService.draw.text(ctx, "+20 Health", Math.max(transform.position.x, transform.position.x - camX), transform.position.y, "45px", "#cf000f", "Permanent Marker")
-            canvasService.draw.rectangle(ctx, Math.max(transform.position.x, transform.position.x - camX), transform.position.y, 235, 50, "rgba(0, 0, 0, 0.8)")
+            ctx.drawImage(img, currentFrame*frameWidth, 0, frameWidth, frameHeight, Math.max(transform.position.x, transform.position.x-camX), transform.position.y, frameWidth, frameHeight);
         }
         
         if (entity.tag === "bar") {
