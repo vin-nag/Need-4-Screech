f8f8f5a2db959739cb29b3ed454022c1e80914e9
Alaa Ibrahim <Loulyta@Loulytas-Mac.local>
2019-04-11 21:23:07 -0230

    Changed the size fo the mini seal default animations.

--- backend/game_engine/default_entity_models.js
+++ backend/game_engine/default_entity_models.js
@@ -163,9 +163,9 @@ class EntityModels {
         enemy_entity.addComponent(components.CHealth(2, 2, false, false));
         enemy_entity.addComponent(components.CAnimation('mini_seal_walk', 5, 0, 0.25));
         enemy_entity.addComponent(components.CTransform(new Vector(x, y), new Vector(x, y), 1, new Vector(0, 0), 0));
-        enemy_entity.addComponent(components.CBoundingBox(new Vector(64, 120), new Vector(32, 60)));
+        enemy_entity.addComponent(components.CBoundingBox(new Vector(64, 64), new Vector(32, 32)));
         enemy_entity.addComponent(components.CEnemyAI("melee",  5, config.enemy.melee.roamDistance, 5000, config.enemy.melee.sight, false, false, new Vector(0, 0), true));
-        enemy_entity.addComponent(components.CEnemyAnim("mini_seal_walk", 5, "mini_seal_attack", 6, "mini_seal_die", 5));
+        enemy_entity.addComponent(components.CEnemyAnim("mini_seal_walk", 5, "mini_seal_attack", 9, "mini_seal_die", 6));
         enemy_entity.addComponent(components.CState("grounded"));
     };
 
