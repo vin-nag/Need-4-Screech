0b910aea231ab6e4ebfa905bd3a5ad7d99fb98cf
Moustafa Elsisy <spaceflyby@gmail.com>
2019-04-13 14:30:29 -0230

    Added support for a local mongodb server
    * Added a db-snapshot folder of our remote cluster
    
    * Updated the `db.js` service to handle connection strings without a username or password
    
    * Updated the README file to include the new instructions
    
    * Updated `config-template` to support a local db server by default

--- backend/services/db.js
+++ backend/services/db.js
@@ -3,7 +3,11 @@ const mongojs = require("mongojs")
 
 const { user, password, clusterString, database, options } = config.db
 const queryParams = Object.keys(options).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(options[k])}`).join('&')
-const mongoUrl = `mongodb://${user}:${password}@${clusterString}/${database}?${queryParams}`
+
+const mongoUrl = (user != null && password != null) ? 
+                 `mongodb://${user}:${password}@${clusterString}/${database}?${queryParams}` :
+                 `mongodb://${clusterString}/${database}?${queryParams}`
+                 
 const db = mongojs(mongoUrl, ['users', 'levels'])
 
 module.exports = {
